e75c6f03391f904731222d15c8ddb5313fcd5cc0
pinusc <giuseppe.stelluto@gmail.com>
2016-01-17 14:30:17 +0100

    make object updatable

--- src/js/box.js
+++ src/js/box.js
@@ -1,13 +1,21 @@
 function Box(parentBox, x, y, text){
     this.children = [];
     this.parentBox = parentBox;
-    x = x || 0;
-    y = y || 0;
-    this.text = text || "Lorem Ipsum";
+
+    this.properties = {
+        "color": "red",
+        "stroke": "#000000",
+        "text": text || "Lorem Ipsum",
+        "x": x || 0,
+        "y": y || 0,
+        "height": 50,
+        "width": 100,
+        "arrow": null
+    };
+    this.updateArrow();
     this.shape = new createjs.Shape();
-    this.shape.x = x;
-    this.shape.y = y;
-    this.shape.graphics.beginFill("red").beginStroke("#000000").drawRect(0, 0, 40, 40);
+
+    this.update();
     // new createjs.Shape(this.shape.graphics).graphics.beginFill("red").drawCircle(10, 10, 20);
     stage.addChild(this.shape);
     stage.update();
@@ -19,9 +27,28 @@ function Box(parentBox, x, y, text){
     }
 }
 
+Box.prototype.update = function(){
+    var p = this.properties;
+    var s = this.shape;
+    console.log(p);
+    s.graphics.clear();  // we need to redraw everything
+    s.x = p.x;
+    s.y = p.y;
+    s.graphics.beginFill(p.color);
+    s.graphics.beginStroke(p.stroke);
+    s.graphics.drawRect(0, 0, p.width, p.height);
+    stage.update();
+};
+
 Box.prototype.addChild = function(text){
     child = new Box(this, this.shape.x, this.shape.y + 100, text);
 
     this.children.push(child);
 };
+
+Box.prototype.updateArrow = function() {
+    if (! this.parentBox) return;  // first box doesn't have an arrow
+    this.arrow = new createjs.Shape();
+    this.arrow.x = this.parentBox.shape.x;
+    this.arrow.y = this.parentBox.shape.y;
 };
