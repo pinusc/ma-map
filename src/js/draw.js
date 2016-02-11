04893acbe14e2a42d0735ddf9447a4bdac374311
pinusc <giuseppe.stelluto@gmail.com>
2016-02-11 20:41:19 +0100

    Move Segment.draw to draw(item)

--- src/js/draw.js
+++ src/js/draw.js
@@ -6,10 +6,14 @@ function draw(item, update) {
     }
     var g = item.shape.graphics;
 
-
     if (item instanceof Point) {
         g.beginFill("black");
         g.drawCircle(item.x, item.y, 5);
+    } else if (item instanceof Segment) {
+        g.beginStroke("black");
+        g.moveTo(item.p1.x, item.p1.y);
+        g.lineTo(item.p2.x, item.p2.y);
+        g.endStroke();
     }
     world.addChild(item.shape);
     if(update) 
--- src/js/math/segment.js
+++ src/js/math/segment.js
@@ -4,18 +4,5 @@ function Segment(p1, p2){
 }
 
 Segment.prototype.draw = function(graphics) {
-    if (graphics) {
-        this.shape = new createjs.Shape();
-    } else {
-        this.shape = new createjs.Shape(graphics);
-    }
-    var g = this.shape.graphics;
-    g.beginStroke("black");
-
-    g.moveTo(this.p1.x, this.p1.y);
-    g.lineTo(this.p2.x, this.p2.y);
-    g.endStroke();
-
-    world.addChild(this.shape);
-    stage.update();
+    draw(this, true);
 };
